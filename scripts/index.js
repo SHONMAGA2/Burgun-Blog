86f429fed6fd97f5c60fb749f04a72eb353c28bc
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-04 02:33:40 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,17 @@
 const container = document.getElementById("cont-sect");
 
-console.log("script loaded container",container);
-
 fetch("./post.json")
-.then(response => response.json())
-.then(posts => {
-    posts.forEach(post =>{
-    const article= document.createElement("article");
-
-    article.innerHTML = `<h2>${post.title}</h2>
-    <p><strong>By:</strong> ${post.posted_by} |          <strong>Date:</strong> ${post.date}</p>
-
-    <p>${post.content}</p>
-    `;
-
-container.appendChild(article);
-});
-
-})
-.catch(error => console.error("Error parsing Json",error));
+  .then(response => response.json())
+  .then(posts => {
+      posts.forEach(post => {
+          const article = document.createElement("article");
+          article.innerHTML = `
+              <img src="${post.image}" alt="${post.title}" class="post-image">
+              <h2>${post.title}</h2>
+              <p><strong>By:</strong> ${post.posted_by} | <strong>Date:</strong> ${post.date}</p>
+              <p>${post.content}</p>
+          `;
+          container.appendChild(article);
+      });
+  })
+  .catch(error => console.error("Error parsing JSON:", error));
